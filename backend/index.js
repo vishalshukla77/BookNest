9f0a79d0c57103cf7e7661fb0d3afea07a363469
vishalshukla77 <121348446+vishalshukla77@users.noreply.github.com>
2025-04-14 12:57:32 +0530

    final commit

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3000;
 // Middleware
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ['http://localhost:5173','https://book-nest-iffs.vercel.app'],
     credentials: true
 }));
 
